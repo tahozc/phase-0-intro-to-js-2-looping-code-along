043b172bebfa9d8e3f172feadfab23d5d22d2c0e
Taha <tahaozcilingir@msn.com>
2023-06-04 21:27:16 -0400

    Uses for command and logs numbers

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const countDown = (num) => {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+function writeCards(array, string) {
+    const newAra = []
+    for (let a = 0; a < array.length; a++) newAra.push(`Thank you, ${array[a]}, for the wonderful ${string} gift!`);
+    return newAra
+}
+
+
+
